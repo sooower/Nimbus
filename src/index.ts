b3abea7b4385516f21bc4f4f49b844dbb14612e9
Jian Liu <jian.liu@fomopay.com>
2023-11-16 10:54:40 +0800

    feat: Add express with swagger ui to show openapi docs
    (cherry picked from commit 59d4b58a486d11afc29663a95321bef2eb965ca7)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-(() => {
-    const msg: string = "Hello World";
-    console.log(msg);
-})();
+import express, { Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+
+const app = express();
+const port = 3000;
+const swaggerDocument = JSON.parse(fs.readFileSync("/Users/Sower/Projects/FOMO/api.fomoremit.net/docs.json", "utf-8"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/api/sample", (req: Request, res: Response) => {
+    res.send("Sample API response");
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
