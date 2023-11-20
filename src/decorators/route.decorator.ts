ff0aab6ace72f4bb5a01763380ce38eeb95bc850
Jian Liu <jian.liu@fomopay.com>
2023-11-20 16:22:46 +0800

    fix: Handle compatibility of symbol "/" in url
    (cherry picked from commit 622d5bc7a83d0089effe93ec7b05059afc04801d)

--- src/decorators/route.decorator.ts
+++ src/decorators/route.decorator.ts
@@ -4,9 +4,17 @@ export const ROUTER_PREFIX = "router_prefix";
 export const ROUTER_PATH = "router_path";
 
 export function Handler(prefix?: string): ClassDecorator {
-    const routePath = prefix == "/" || !prefix ? "" : prefix;
+    let routePrefix = prefix == "/" || !prefix ? "" : prefix; // FIXME
+
+    if (!routePrefix.startsWith("/")) {
+        routePrefix = "/" + routePrefix;
+    }
+    if (routePrefix.endsWith("/")) {
+        routePrefix = routePrefix.slice(0, routePrefix.length - 1);
+    }
+
     return function (target: any) {
-        Reflect.defineMetadata(ROUTER_PREFIX, routePath, target.prototype);
+        Reflect.defineMetadata(ROUTER_PREFIX, routePrefix, target.prototype);
     };
 }
 
@@ -14,10 +22,10 @@ function createRouteDecorator(method: string) {
     return function (
         path?: string,
         ...middlewares: ((req: Request, res: Response, next: NextFunction) => Promise<void>)[]
-    ) {
-        return function (target: any, propertyKey: string, _: PropertyDescriptor) {
+    ): MethodDecorator {
+        return function (target: any, propertyKey: string | symbol, _: PropertyDescriptor) {
+            const routePath = path == "" || !path ? "/" : path.startsWith("/") ? path : "/" + path;
             const router = Reflect.getMetadata(ROUTER_PATH, target) || Router();
-            const routePath = path == "" || !path ? "/" : path;
             router[method](routePath, ...middlewares, target[propertyKey]);
             Reflect.defineMetadata(ROUTER_PATH, router, target);
         };
