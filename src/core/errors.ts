cd4de7cb27f3b99db08a4b391cdf1e11eba06310
Jian Liu <jian.liu@fomopay.com>
2023-12-06 21:51:30 +0800

    feat: add ServiceError

---
+++ src/core/errors.ts
@@ -0,0 +1,12 @@
+export class ServiceError extends Error {
+    status: number;
+    code: string;
+    message: string;
+
+    constructor(status: number, code: string, message: string) {
+        super();
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
--- src/core/middlewares/error.middleware.ts
+++ src/core/middlewares/error.middleware.ts
@@ -1,6 +1,12 @@
-import { Req, Res, Next } from "../types/common";
+import { ServiceError } from "../errors";
+import { Next, Req, Res } from "../types/common";
 
-export function errorMiddleware(err: any, req: Req, res: Res, next: Next) {
+export function errorMiddleware(
+    err: ServiceError,
+    req: Req,
+    res: Res,
+    next: Next,
+) {
     console.error(err.stack);
 
     res.status(err.status || 500).json({
