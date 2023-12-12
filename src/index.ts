db49b2f086dfec3863160fe50437a1245754499d
Jian Liu <jian.liu@fomopay.com>
2023-12-12 11:23:57 +0800

    chore: remove swagger-ui-express

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import "reflect-metadata";
 
 import bodyParser from "body-parser";
 import express from "express";
-import swaggerUi from "swagger-ui-express";
 
-import apiDocuments from "../../../FOMO/Codes/api.fomoremit.net/api-docs.json";
 import { corsMiddleware } from "./core/middlewares/cors.middleware";
 import { errorMiddleware } from "./core/middlewares/error.middleware";
 import { autoRegisterRoutes, globalConfig } from "./core/utils";
@@ -16,8 +14,6 @@ app.use(corsMiddleware);
 
 // Parse request body
 app.use(bodyParser.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocuments));
 
 // Auto register routes
 autoRegisterRoutes(app);
