5faf84fe97dbdd1cebfb68239a9f4a6df13f4d57
Jian Liu <jian.liu@fomopay.com>
2023-12-24 16:52:41 +0800

    fix: errMiddleware

--- src/core/middlewares/errorMiddleware.ts
+++ src/core/middlewares/errorMiddleware.ts
@@ -2,15 +2,10 @@ import { logger } from "../components/logger";
 import { ServiceError } from "../errors";
 import { Next, Req, Res } from "../types";
 
-export function errorMiddleware(
-    err: ServiceError,
-    req: Req,
-    res: Res,
-    next: Next,
-) {
-    logger.error(`[${err.requestId}]`, err);
-
+export function errorMiddleware(err: Error, req: Req, res: Res, next: Next) {
     if (err instanceof ServiceError) {
+        logger.error(`[${err.requestId}]`, err);
+
         const { status, requestId, message } = err;
 
         return res.status(status).json({
