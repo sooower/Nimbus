549ad89f913cfca51fcba311861ce6bb99e83ff6
Jian Liu <jian.liu@fomopay.com>
2023-12-26 16:51:46 +0800

    fix: update route "/ping" to non-authorization

--- src/controllers/pingController.ts
+++ src/controllers/pingController.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from "@/core/decorators/routeDecorator";
 import dayjs from "dayjs";
+
+import { NonAuth } from "@/core/decorators/nonAuthDecorator";
+import { Controller, Get } from "@/core/decorators/routeDecorator";
 
 @Controller("/ping")
 export class PingController {
     @Get("/")
+    @NonAuth
     async get() {
         return `Now time: ${dayjs().format("YYYY/MM/DD HH:mm:ssZ")}`;
     }
