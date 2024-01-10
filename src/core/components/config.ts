a47e981953818873688305acd0f9bc31a8193317
Jian Liu <jian.liu@fomopay.com>
2024-01-10 20:34:25 +0800

    chore: rename default config file

--- src/core/components/config.ts
+++ src/core/components/config.ts
@@ -5,10 +5,10 @@ import { Objects } from "../utils/objects";
 
 function loadConfig(): any {
     const { env, baseDir, ext } = Commons.getEnvBaseDirAndExt();
-    const defaultConfig = require(path.resolve(`${baseDir}/config/config.${ext}`)).default;
+    const sharedConfig = require(path.resolve(`${baseDir}/config/config.shared.${ext}`)).default;
     const envConfig = require(path.resolve(`${baseDir}/config/config.${env}.${ext}`)).default;
 
-    return Objects.merge(envConfig, defaultConfig);
+    return Objects.merge(envConfig, sharedConfig);
 }
 
 export const globalConfig = loadConfig();
